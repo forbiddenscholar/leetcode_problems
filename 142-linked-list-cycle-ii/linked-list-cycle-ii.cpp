ceea90d1be31bb97777680d76e8ab18fcc7234ea
Mohit Deharkar <mohitdeharkar@gmail.com>
2026-05-15 20:35:02 +0530

    Time: 10 ms (45.82%) | Memory: 11.2 MB (99.00%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,7 +9,23 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        // using the bruteforce apporach using unordered_set
+        /*
+        unordered_set <ListNode*> visited;
+        // loop until we reach the end of ll (Null)
+        while(head != NULL){
+            // if current node is visited, we got the cycle's head
+            if(visited.find(head) != visited.end())
+                return head;
+            
+            // else visiting first time, so insert in the set
+            visited.insert(head);
+            head = head->next;
+        }
 
+        // if exits off the loop, there is no cycle
+        return NULL;
+        */
         // using the optimal approach now -- tortoise and hare algo
         ListNode* fast = head;
         ListNode* slow = head;
