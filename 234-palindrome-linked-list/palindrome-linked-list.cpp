1b6044fc05ceaf06d47e2427decd84371f4f5d93
Mohit Deharkar <mohitdeharkar@gmail.com>
2026-05-16 22:26:47 +0530

    Time: 7 ms (27.62%) | Memory: 120.3 MB (49.50%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,8 +9,29 @@
  * };
  */
 class Solution {
+private:
+    ListNode* findMid(ListNode* head){
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* revLL(ListNode* head){
+        if(head->next == NULL)
+            return head;
+        ListNode* newHead = revLL(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+
 public:
     bool isPalindrome(ListNode* head) {
+        /*
         // trying the bruteforce approach using stack
         stack <int> st;
         ListNode* temp = head;
@@ -26,6 +47,31 @@ public:
         
             head = head->next;
             st.pop();            
+        }
+
+        return true;
+        */
+        
+        // the optimal approach :
+        // 1. find middle using tortoise hare
+        // 2. reverse the LL from the middle
+        // 3. now check the head against new head from the reversed
+
+        ListNode* mid = findMid(head);
+        ListNode* newHead = revLL(mid);
+
+        // now check the new LL
+        // 1->2->3->4->5, 3 is mid, put a temp1 at 1 and temp2 at newhead3
+        ListNode* temp1 = head;
+        ListNode* temp2 = newHead;
+
+        while(temp2 != NULL){
+            if(temp1->val != temp2->val){
+                return false;
+            }
+
+            temp1 = temp1->next;
+            temp2 = temp2->next;
         }
 
         return true;
