9aa7c2e377f26f0c52f6229fd9f689f3c24dafed
Mohit Deharkar <mohitdeharkar@gmail.com>
2026-06-10 17:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (43.60%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -14,6 +14,26 @@ public:
     int countNodes(TreeNode* root) {
         if(root == NULL)
             return 0;
+        int lh = findHeightLeft(root);
+        int rh = findHeightRight(root);
+        if(lh == rh)
+            return (1<<lh) - 1;
         return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+    int findHeightLeft(TreeNode* root){
+        int height = 0;
+        while(root){
+            height++;
+            root = root->left;
+        }
+        return height;
+    }
+    int findHeightRight(TreeNode* root){
+        int height = 0;
+        while(root){
+            height++;
+            root = root->right;
+        }
+        return height;
     }
 };
